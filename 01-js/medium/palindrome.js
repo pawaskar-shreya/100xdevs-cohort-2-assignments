6828a3810c8cbe17dfc4d653d28a272f3a0d070e
pawaskar-shreya <shreya.pawaskar.main@gmail.com>
2025-09-23 22:07:35 +0530

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,29 @@
 */
 
 function isPalindrome(str) {
+  let s = str.toUpperCase();
+  let n = str.length;
+  let skip = [',', '?', '!', '.', ' '];
+  let i = 0;
+  let j = n-1-i;
+
+  while(j>=i) {
+
+    while(i<j && skip.includes(s[i])) {
+      i++
+    }
+
+    while(j>i && skip.includes(s[j])) {
+      j--;
+    }
+
+    if(s[i] != s[j]) {
+      return false;
+    }
+
+    i++;
+    j--;
+  }
   return true;
 }
 
