d28023d09e363d301f96770a09a21ab52aea4078
pawaskar-shreya <shreya.pawaskar.main@gmail.com>
2025-09-23 19:08:12 +0530

    anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,18 @@
 
 function isAnagram(str1, str2) {
 
+  // Method 1: split every character, sort it anc then compare
+
+   let arr1 = str1.toLowerCase().split('').sort();
+   let arr2 = str2.toLowerCase().split('').sort();
+
+
+   for(let i=0; i<arr1.length; i++){
+    if(arr1[i] != arr2[i]) {
+      return false;
+    }
+}
+   return true;
 }
 
 module.exports = isAnagram;
