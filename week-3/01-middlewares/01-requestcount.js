dad1aa3008243742ca3a8c06d2a2b3ef2b43de02
pawaskar-shreya <shreya.pawaskar.main@gmail.com>
2025-09-27 21:07:59 +0530

    01-requestcount.js

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -10,6 +10,14 @@ let requestCount = 0;
 // maintain a count of the number of requests made to the server in the global
 // requestCount variable
 
+
+function calculateRequest(req, res, next) {                                 // Dont forget the args
+  requestCount++;                                 
+  next();                                       // Dont forget to call next(), or it will be hung here and server will become irresponsive
+}
+
+app.use(calculateRequest);
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
