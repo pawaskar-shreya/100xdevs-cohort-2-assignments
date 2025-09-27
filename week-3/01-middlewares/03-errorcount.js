45a625e34d04f6296ebc6aeced8151e850099399
pawaskar-shreya <shreya.pawaskar.main@gmail.com>
2025-09-28 00:07:21 +0530

    03-errorcount.js

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,28 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+// Exception Middlewares
+
+// The route not found middleware should come before the exception middlewares that will catch an error.
+// Middlewares are executed in the order they are defined for app.use(). They get added as callback functions in the routes in the order in which we define them here. And the same order is followed for execution. 
+// This is imp becauase if we write the "route not found" after "err catching middleware", the "err catching middleware" will be checked first. 
+// The "route not present" does not raise an exception. Hence, the "err catching middleware" will not even be executed as it thinks there is no exception. Since this first middleware is not called, the next() inside it wont be called. Hence none of the middlewares get called. 
+// Hence it is imp that we call the exception middelwares in correct order.
+app.use(function(req, res, next) {
+  res.status(404).json({
+    msg : "Bad route :("
+  })
+})
+
+app.use(function(err, req, res, next) {
+  errorCount++;
+  res.status(404).json({
+    msg : "There is an exception"
+  });
+
+  next();
+})
+
+app.listen(3000);
+
 module.exports = app;
